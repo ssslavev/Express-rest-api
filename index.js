f17d47c1d8a5afd4a12be1475c105be2fdd5f278
ssslavev <s.s.slavev@hotmail.com>
2019-06-03 17:39:34 +0300

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
 const bodyParser = require('body-parser');
